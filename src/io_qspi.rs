2ea7564f1e371583ffe2935f2b545fca70d9c532
Evan Older <evanmolder@gmail.com>
2021-02-01 15:39:26 -0500

    Revert "Revert "Patched SVD with svdtools to make QSPI/GPIO ctrl/stat registers arrays.""

--- src/io_qspi.rs
+++ src/io_qspi.rs
@@ -1,30 +1,18 @@
 #[doc = r"Register block"]
 #[repr(C)]
 pub struct RegisterBlock {
-    #[doc = "0x00 - GPIO status"]
-    pub gpio_qspi_sclk_status: GPIO_QSPI_SCLK_STATUS,
-    #[doc = "0x04 - GPIO control including function select and overrides."]
-    pub gpio_qspi_sclk_ctrl: GPIO_QSPI_SCLK_CTRL,
-    #[doc = "0x08 - GPIO status"]
-    pub gpio_qspi_ss_status: GPIO_QSPI_SS_STATUS,
-    #[doc = "0x0c - GPIO control including function select and overrides."]
-    pub gpio_qspi_ss_ctrl: GPIO_QSPI_SS_CTRL,
-    #[doc = "0x10 - GPIO status"]
-    pub gpio_qspi_sd0_status: GPIO_QSPI_SD0_STATUS,
-    #[doc = "0x14 - GPIO control including function select and overrides."]
-    pub gpio_qspi_sd0_ctrl: GPIO_QSPI_SD0_CTRL,
-    #[doc = "0x18 - GPIO status"]
-    pub gpio_qspi_sd1_status: GPIO_QSPI_SD1_STATUS,
-    #[doc = "0x1c - GPIO control including function select and overrides."]
-    pub gpio_qspi_sd1_ctrl: GPIO_QSPI_SD1_CTRL,
-    #[doc = "0x20 - GPIO status"]
-    pub gpio_qspi_sd2_status: GPIO_QSPI_SD2_STATUS,
-    #[doc = "0x24 - GPIO control including function select and overrides."]
-    pub gpio_qspi_sd2_ctrl: GPIO_QSPI_SD2_CTRL,
-    #[doc = "0x28 - GPIO status"]
-    pub gpio_qspi_sd3_status: GPIO_QSPI_SD3_STATUS,
-    #[doc = "0x2c - GPIO control including function select and overrides."]
-    pub gpio_qspi_sd3_ctrl: GPIO_QSPI_SD3_CTRL,
+    #[doc = "0x00 - Cluster GPIO_QSPI%s, containing GPIO_QSPI_*_STATUS, GPIO_QSPI_*_CTRL"]
+    pub gpio_qspisclk: GPIO_QSPI,
+    #[doc = "0x08 - Cluster GPIO_QSPI%s, containing GPIO_QSPI_*_STATUS, GPIO_QSPI_*_CTRL"]
+    pub gpio_qspiss: GPIO_QSPI,
+    #[doc = "0x10 - Cluster GPIO_QSPI%s, containing GPIO_QSPI_*_STATUS, GPIO_QSPI_*_CTRL"]
+    pub gpio_qspisd0: GPIO_QSPI,
+    #[doc = "0x18 - Cluster GPIO_QSPI%s, containing GPIO_QSPI_*_STATUS, GPIO_QSPI_*_CTRL"]
+    pub gpio_qspisd1: GPIO_QSPI,
+    #[doc = "0x20 - Cluster GPIO_QSPI%s, containing GPIO_QSPI_*_STATUS, GPIO_QSPI_*_CTRL"]
+    pub gpio_qspisd2: GPIO_QSPI,
+    #[doc = "0x28 - Cluster GPIO_QSPI%s, containing GPIO_QSPI_*_STATUS, GPIO_QSPI_*_CTRL"]
+    pub gpio_qspisd3: GPIO_QSPI,
     #[doc = "0x30 - Raw Interrupts"]
     pub intr: INTR,
     #[doc = "0x34 - Interrupt Enable for proc0"]
@@ -46,126 +34,17 @@ pub struct RegisterBlock {
     #[doc = "0x54 - Interrupt status after masking & forcing for dormant_wake"]
     pub dormant_wake_ints: DORMANT_WAKE_INTS,
 }
-#[doc = "GPIO status\n\nThis register you can [`read`](crate::generic::Reg::read). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_sclk_status](gpio_qspi_sclk_status) module"]
-pub type GPIO_QSPI_SCLK_STATUS = crate::Reg<u32, _GPIO_QSPI_SCLK_STATUS>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SCLK_STATUS;
-#[doc = "`read()` method returns [gpio_qspi_sclk_status::R](gpio_qspi_sclk_status::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SCLK_STATUS {}
-#[doc = "GPIO status"]
-pub mod gpio_qspi_sclk_status;
-#[doc = "GPIO control including function select and overrides.\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_sclk_ctrl](gpio_qspi_sclk_ctrl) module"]
-pub type GPIO_QSPI_SCLK_CTRL = crate::Reg<u32, _GPIO_QSPI_SCLK_CTRL>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SCLK_CTRL;
-#[doc = "`read()` method returns [gpio_qspi_sclk_ctrl::R](gpio_qspi_sclk_ctrl::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SCLK_CTRL {}
-#[doc = "`write(|w| ..)` method takes [gpio_qspi_sclk_ctrl::W](gpio_qspi_sclk_ctrl::W) writer structure"]
-impl crate::Writable for GPIO_QSPI_SCLK_CTRL {}
-#[doc = "GPIO control including function select and overrides."]
-pub mod gpio_qspi_sclk_ctrl;
-#[doc = "GPIO status\n\nThis register you can [`read`](crate::generic::Reg::read). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_ss_status](gpio_qspi_ss_status) module"]
-pub type GPIO_QSPI_SS_STATUS = crate::Reg<u32, _GPIO_QSPI_SS_STATUS>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SS_STATUS;
-#[doc = "`read()` method returns [gpio_qspi_ss_status::R](gpio_qspi_ss_status::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SS_STATUS {}
-#[doc = "GPIO status"]
-pub mod gpio_qspi_ss_status;
-#[doc = "GPIO control including function select and overrides.\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_ss_ctrl](gpio_qspi_ss_ctrl) module"]
-pub type GPIO_QSPI_SS_CTRL = crate::Reg<u32, _GPIO_QSPI_SS_CTRL>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SS_CTRL;
-#[doc = "`read()` method returns [gpio_qspi_ss_ctrl::R](gpio_qspi_ss_ctrl::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SS_CTRL {}
-#[doc = "`write(|w| ..)` method takes [gpio_qspi_ss_ctrl::W](gpio_qspi_ss_ctrl::W) writer structure"]
-impl crate::Writable for GPIO_QSPI_SS_CTRL {}
-#[doc = "GPIO control including function select and overrides."]
-pub mod gpio_qspi_ss_ctrl;
-#[doc = "GPIO status\n\nThis register you can [`read`](crate::generic::Reg::read). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_sd0_status](gpio_qspi_sd0_status) module"]
-pub type GPIO_QSPI_SD0_STATUS = crate::Reg<u32, _GPIO_QSPI_SD0_STATUS>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SD0_STATUS;
-#[doc = "`read()` method returns [gpio_qspi_sd0_status::R](gpio_qspi_sd0_status::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SD0_STATUS {}
-#[doc = "GPIO status"]
-pub mod gpio_qspi_sd0_status;
-#[doc = "GPIO control including function select and overrides.\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_sd0_ctrl](gpio_qspi_sd0_ctrl) module"]
-pub type GPIO_QSPI_SD0_CTRL = crate::Reg<u32, _GPIO_QSPI_SD0_CTRL>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SD0_CTRL;
-#[doc = "`read()` method returns [gpio_qspi_sd0_ctrl::R](gpio_qspi_sd0_ctrl::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SD0_CTRL {}
-#[doc = "`write(|w| ..)` method takes [gpio_qspi_sd0_ctrl::W](gpio_qspi_sd0_ctrl::W) writer structure"]
-impl crate::Writable for GPIO_QSPI_SD0_CTRL {}
-#[doc = "GPIO control including function select and overrides."]
-pub mod gpio_qspi_sd0_ctrl;
-#[doc = "GPIO status\n\nThis register you can [`read`](crate::generic::Reg::read). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_sd1_status](gpio_qspi_sd1_status) module"]
-pub type GPIO_QSPI_SD1_STATUS = crate::Reg<u32, _GPIO_QSPI_SD1_STATUS>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SD1_STATUS;
-#[doc = "`read()` method returns [gpio_qspi_sd1_status::R](gpio_qspi_sd1_status::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SD1_STATUS {}
-#[doc = "GPIO status"]
-pub mod gpio_qspi_sd1_status;
-#[doc = "GPIO control including function select and overrides.\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_sd1_ctrl](gpio_qspi_sd1_ctrl) module"]
-pub type GPIO_QSPI_SD1_CTRL = crate::Reg<u32, _GPIO_QSPI_SD1_CTRL>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SD1_CTRL;
-#[doc = "`read()` method returns [gpio_qspi_sd1_ctrl::R](gpio_qspi_sd1_ctrl::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SD1_CTRL {}
-#[doc = "`write(|w| ..)` method takes [gpio_qspi_sd1_ctrl::W](gpio_qspi_sd1_ctrl::W) writer structure"]
-impl crate::Writable for GPIO_QSPI_SD1_CTRL {}
-#[doc = "GPIO control including function select and overrides."]
-pub mod gpio_qspi_sd1_ctrl;
-#[doc = "GPIO status\n\nThis register you can [`read`](crate::generic::Reg::read). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_sd2_status](gpio_qspi_sd2_status) module"]
-pub type GPIO_QSPI_SD2_STATUS = crate::Reg<u32, _GPIO_QSPI_SD2_STATUS>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SD2_STATUS;
-#[doc = "`read()` method returns [gpio_qspi_sd2_status::R](gpio_qspi_sd2_status::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SD2_STATUS {}
-#[doc = "GPIO status"]
-pub mod gpio_qspi_sd2_status;
-#[doc = "GPIO control including function select and overrides.\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_sd2_ctrl](gpio_qspi_sd2_ctrl) module"]
-pub type GPIO_QSPI_SD2_CTRL = crate::Reg<u32, _GPIO_QSPI_SD2_CTRL>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SD2_CTRL;
-#[doc = "`read()` method returns [gpio_qspi_sd2_ctrl::R](gpio_qspi_sd2_ctrl::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SD2_CTRL {}
-#[doc = "`write(|w| ..)` method takes [gpio_qspi_sd2_ctrl::W](gpio_qspi_sd2_ctrl::W) writer structure"]
-impl crate::Writable for GPIO_QSPI_SD2_CTRL {}
-#[doc = "GPIO control including function select and overrides."]
-pub mod gpio_qspi_sd2_ctrl;
-#[doc = "GPIO status\n\nThis register you can [`read`](crate::generic::Reg::read). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_sd3_status](gpio_qspi_sd3_status) module"]
-pub type GPIO_QSPI_SD3_STATUS = crate::Reg<u32, _GPIO_QSPI_SD3_STATUS>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SD3_STATUS;
-#[doc = "`read()` method returns [gpio_qspi_sd3_status::R](gpio_qspi_sd3_status::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SD3_STATUS {}
-#[doc = "GPIO status"]
-pub mod gpio_qspi_sd3_status;
-#[doc = "GPIO control including function select and overrides.\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [gpio_qspi_sd3_ctrl](gpio_qspi_sd3_ctrl) module"]
-pub type GPIO_QSPI_SD3_CTRL = crate::Reg<u32, _GPIO_QSPI_SD3_CTRL>;
-#[allow(missing_docs)]
-#[doc(hidden)]
-pub struct _GPIO_QSPI_SD3_CTRL;
-#[doc = "`read()` method returns [gpio_qspi_sd3_ctrl::R](gpio_qspi_sd3_ctrl::R) reader structure"]
-impl crate::Readable for GPIO_QSPI_SD3_CTRL {}
-#[doc = "`write(|w| ..)` method takes [gpio_qspi_sd3_ctrl::W](gpio_qspi_sd3_ctrl::W) writer structure"]
-impl crate::Writable for GPIO_QSPI_SD3_CTRL {}
-#[doc = "GPIO control including function select and overrides."]
-pub mod gpio_qspi_sd3_ctrl;
+#[doc = r"Register block"]
+#[repr(C)]
+pub struct GPIO_QSPI {
+    #[doc = "0x00 - GPIO status"]
+    pub gpio_qspi__status: self::gpio_qspi::GPIO_QSPI__STATUS,
+    #[doc = "0x04 - GPIO control including function select and overrides."]
+    pub gpio_qspi__ctrl: self::gpio_qspi::GPIO_QSPI__CTRL,
+}
+#[doc = r"Register block"]
+#[doc = "Cluster GPIO_QSPI%s, containing GPIO_QSPI_*_STATUS, GPIO_QSPI_*_CTRL"]
+pub mod gpio_qspi;
 #[doc = "Raw Interrupts\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [intr](intr) module"]
 pub type INTR = crate::Reg<u32, _INTR>;
 #[allow(missing_docs)]
