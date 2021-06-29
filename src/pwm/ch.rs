30264020541b2e43ff21532ffd80de6ed448272e
Andrea Nall <anall@andreanall.com>
2021-06-28 20:12:56 -0500

    forgot to add new files

---
+++ src/pwm/ch.rs
@@ -0,0 +1,55 @@
+#[doc = "Counter compare values\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [cc](cc) module"]
+pub type CC = crate::Reg<u32, _CC>;
+#[allow(missing_docs)]
+#[doc(hidden)]
+pub struct _CC;
+#[doc = "`read()` method returns [cc::R](cc::R) reader structure"]
+impl crate::Readable for CC {}
+#[doc = "`write(|w| ..)` method takes [cc::W](cc::W) writer structure"]
+impl crate::Writable for CC {}
+#[doc = "Counter compare values"]
+pub mod cc;
+#[doc = "Control and status register\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [csr](csr) module"]
+pub type CSR = crate::Reg<u32, _CSR>;
+#[allow(missing_docs)]
+#[doc(hidden)]
+pub struct _CSR;
+#[doc = "`read()` method returns [csr::R](csr::R) reader structure"]
+impl crate::Readable for CSR {}
+#[doc = "`write(|w| ..)` method takes [csr::W](csr::W) writer structure"]
+impl crate::Writable for CSR {}
+#[doc = "Control and status register"]
+pub mod csr;
+#[doc = "Direct access to the PWM counter\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [ctr](ctr) module"]
+pub type CTR = crate::Reg<u32, _CTR>;
+#[allow(missing_docs)]
+#[doc(hidden)]
+pub struct _CTR;
+#[doc = "`read()` method returns [ctr::R](ctr::R) reader structure"]
+impl crate::Readable for CTR {}
+#[doc = "`write(|w| ..)` method takes [ctr::W](ctr::W) writer structure"]
+impl crate::Writable for CTR {}
+#[doc = "Direct access to the PWM counter"]
+pub mod ctr;
+#[doc = "INT and FRAC form a fixed-point fractional number.\\n Counting rate is system clock frequency divided by this number.\\n Fractional division uses simple 1st-order sigma-delta.\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [div](div) module"]
+pub type DIV = crate::Reg<u32, _DIV>;
+#[allow(missing_docs)]
+#[doc(hidden)]
+pub struct _DIV;
+#[doc = "`read()` method returns [div::R](div::R) reader structure"]
+impl crate::Readable for DIV {}
+#[doc = "`write(|w| ..)` method takes [div::W](div::W) writer structure"]
+impl crate::Writable for DIV {}
+#[doc = "INT and FRAC form a fixed-point fractional number.\\n Counting rate is system clock frequency divided by this number.\\n Fractional division uses simple 1st-order sigma-delta."]
+pub mod div;
+#[doc = "Counter wrap value\n\nThis register you can [`read`](crate::generic::Reg::read), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [top](top) module"]
+pub type TOP = crate::Reg<u32, _TOP>;
+#[allow(missing_docs)]
+#[doc(hidden)]
+pub struct _TOP;
+#[doc = "`read()` method returns [top::R](top::R) reader structure"]
+impl crate::Readable for TOP {}
+#[doc = "`write(|w| ..)` method takes [top::W](top::W) writer structure"]
+impl crate::Writable for TOP {}
+#[doc = "Counter wrap value"]
+pub mod top;
