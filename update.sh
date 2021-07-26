61324ae866b4bf00935589b63d414447a013a567
Hmvp <hmvp@users.noreply.github.com>
2021-07-26 15:50:08 +0200

    Fix escaped lineendings
    And unknown lints

--- update.sh
+++ update.sh
@@ -23,6 +23,15 @@ cargo fmt
 
 sed -i "/extern crate bare_metal;/d" ./src/lib.rs
 sed -i 's/bare_metal::Nr/cortex_m::interrupt::Nr/g' ./src/lib.rs
+sed -i '/legacy_directory_ownership/d' ./src/lib.rs
+sed -i '/plugin_as_library/d' ./src/lib.rs
+sed -i '/safe_extern_statics/d' ./src/lib.rs
+sed -i '/unions_with_drop_fields/d' ./src/lib.rs
+# Original svd has \n (two chars) in it, which gets converted to "\n" and "\\n" by svd2rust
+# If we convert them to newline characters in the SVD, they don't turn up in markdown so docs suffers
+# So, convert both \\n and \n to [spc] [spc] [newline], then strip the spaces out if there are consecutive [newlines]
+# This means that by the time we're in markdown \n\n becomes new paragraph, and \n becomes a new line
+find src -name '*.rs' -exec sed -i -e 's/\\\\n/\\n/g' -e 's/\\n/  \n/g' -e 's/\n  \n/\n\n/g' {} \;
 
 # Sort specified fields alphanumerically for easier consumption in docs.rs
 ./sortFieldsAlphaNum.sh
