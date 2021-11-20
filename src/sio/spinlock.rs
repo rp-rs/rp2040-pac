617a1ed39d1a0f1eee8cc2b2e01538f366c6980b
9names <60134748+9names@users.noreply.github.com>
2021-11-20 11:50:28 +1100

    Add svd2rust generated files

--- src/sio/spinlock.rs
+++ src/sio/spinlock.rs
@@ -1,22 +1,22 @@
-#[doc = "Register `SPINLOCK22` reader"]
-pub struct R(crate::R<SPINLOCK22_SPEC>);
+#[doc = "Register `SPINLOCK%s` reader"]
+pub struct R(crate::R<SPINLOCK_SPEC>);
 impl core::ops::Deref for R {
-    type Target = crate::R<SPINLOCK22_SPEC>;
+    type Target = crate::R<SPINLOCK_SPEC>;
     #[inline(always)]
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
-impl From<crate::R<SPINLOCK22_SPEC>> for R {
+impl From<crate::R<SPINLOCK_SPEC>> for R {
     #[inline(always)]
-    fn from(reader: crate::R<SPINLOCK22_SPEC>) -> Self {
+    fn from(reader: crate::R<SPINLOCK_SPEC>) -> Self {
         R(reader)
     }
 }
-#[doc = "Register `SPINLOCK22` writer"]
-pub struct W(crate::W<SPINLOCK22_SPEC>);
+#[doc = "Register `SPINLOCK%s` writer"]
+pub struct W(crate::W<SPINLOCK_SPEC>);
 impl core::ops::Deref for W {
-    type Target = crate::W<SPINLOCK22_SPEC>;
+    type Target = crate::W<SPINLOCK_SPEC>;
     #[inline(always)]
     fn deref(&self) -> &Self::Target {
         &self.0
@@ -28,9 +28,9 @@ impl core::ops::DerefMut for W {
         &mut self.0
     }
 }
-impl From<crate::W<SPINLOCK22_SPEC>> for W {
+impl From<crate::W<SPINLOCK_SPEC>> for W {
     #[inline(always)]
-    fn from(writer: crate::W<SPINLOCK22_SPEC>) -> Self {
+    fn from(writer: crate::W<SPINLOCK_SPEC>) -> Self {
         W(writer)
     }
 }
@@ -43,30 +43,30 @@ impl W {
     }
 }
 #[doc = "Reading from a spinlock address will:  
- - Return 0 if lock is already locked  
+ - Return %s if lock is already locked  
  - Otherwise return nonzero, and simultaneously claim the lock  
 
  Writing (any value) releases the lock.  
- If core 0 and core 1 attempt to claim the same lock simultaneously, core 0 wins.  
- The value returned on success is 0x1 << lock number.  
+ If core %s and core 1 attempt to claim the same lock simultaneously, core %s wins.  
+ The value returned on success is %sx1 << lock number.  
 
 This register you can [`read`](crate::generic::Reg::read), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).  
 
-For information about available fields see [spinlock22](index.html) module"]
-pub struct SPINLOCK22_SPEC;
-impl crate::RegisterSpec for SPINLOCK22_SPEC {
+For information about available fields see [spinlock](index.html) module"]
+pub struct SPINLOCK_SPEC;
+impl crate::RegisterSpec for SPINLOCK_SPEC {
     type Ux = u32;
 }
-#[doc = "`read()` method returns [spinlock22::R](R) reader structure"]
-impl crate::Readable for SPINLOCK22_SPEC {
+#[doc = "`read()` method returns [spinlock::R](R) reader structure"]
+impl crate::Readable for SPINLOCK_SPEC {
     type Reader = R;
 }
-#[doc = "`write(|w| ..)` method takes [spinlock22::W](W) writer structure"]
-impl crate::Writable for SPINLOCK22_SPEC {
+#[doc = "`write(|w| ..)` method takes [spinlock::W](W) writer structure"]
+impl crate::Writable for SPINLOCK_SPEC {
     type Writer = W;
 }
-#[doc = "`reset()` method sets SPINLOCK22 to value 0"]
-impl crate::Resettable for SPINLOCK22_SPEC {
+#[doc = "`reset()` method sets SPINLOCK%s to value 0"]
+impl crate::Resettable for SPINLOCK_SPEC {
     #[inline(always)]
     fn reset_value() -> Self::Ux {
         0
