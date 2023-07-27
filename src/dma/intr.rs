d2b6cd01c78bf91116f5a64ae18beb8bd94df985
Jan Niehusmann <jan@gondor.com>
2023-07-27 09:04:44 +0000

    Update rp2040.svd to the version from upstream sdk 1.5.1
    Mainly doc updates. And dma INTR register was changed to read-write (with oneToClear).

--- src/dma/intr.rs
+++ src/dma/intr.rs
@@ -13,6 +13,27 @@ impl From<crate::R<INTR_SPEC>> for R {
         R(reader)
     }
 }
+#[doc = "Register `INTR` writer"]
+pub struct W(crate::W<INTR_SPEC>);
+impl core::ops::Deref for W {
+    type Target = crate::W<INTR_SPEC>;
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl core::ops::DerefMut for W {
+    #[inline(always)]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+impl From<crate::W<INTR_SPEC>> for W {
+    #[inline(always)]
+    fn from(writer: crate::W<INTR_SPEC>) -> Self {
+        W(writer)
+    }
+}
 #[doc = "Field `INTR` reader - Raw interrupt status for DMA Channels 0..15. Bit n corresponds to channel n. Ignores any masking or forcing. Channel interrupts can be cleared by writing a bit mask to INTR, INTS0 or INTS1.  
 
  Channel interrupts can be routed to either of two system-level IRQs based on INTE0 and INTE1.  
@@ -21,6 +42,14 @@ impl From<crate::R<INTR_SPEC>> for R {
 
  It is also valid to ignore this behaviour and just use INTE0/INTS0/IRQ 0."]
 pub type INTR_R = crate::FieldReader<u16>;
+#[doc = "Field `INTR` writer - Raw interrupt status for DMA Channels 0..15. Bit n corresponds to channel n. Ignores any masking or forcing. Channel interrupts can be cleared by writing a bit mask to INTR, INTS0 or INTS1.  
+
+ Channel interrupts can be routed to either of two system-level IRQs based on INTE0 and INTE1.  
+
+ This can be used vector different channel interrupts to different ISRs: this might be done to allow NVIC IRQ preemption for more time-critical channels, or to spread IRQ load across different cores.  
+
+ It is also valid to ignore this behaviour and just use INTE0/INTS0/IRQ 0."]
+pub type INTR_W<'a, const O: u8> = crate::FieldWriter<'a, INTR_SPEC, 16, O, u16>;
 impl R {
     #[doc = "Bits 0:15 - Raw interrupt status for DMA Channels 0..15. Bit n corresponds to channel n. Ignores any masking or forcing. Channel interrupts can be cleared by writing a bit mask to INTR, INTS0 or INTS1.  
 
@@ -34,9 +63,29 @@ impl R {
         INTR_R::new((self.bits & 0xffff) as u16)
     }
 }
+impl W {
+    #[doc = "Bits 0:15 - Raw interrupt status for DMA Channels 0..15. Bit n corresponds to channel n. Ignores any masking or forcing. Channel interrupts can be cleared by writing a bit mask to INTR, INTS0 or INTS1.  
+
+ Channel interrupts can be routed to either of two system-level IRQs based on INTE0 and INTE1.  
+
+ This can be used vector different channel interrupts to different ISRs: this might be done to allow NVIC IRQ preemption for more time-critical channels, or to spread IRQ load across different cores.  
+
+ It is also valid to ignore this behaviour and just use INTE0/INTS0/IRQ 0."]
+    #[inline(always)]
+    #[must_use]
+    pub fn intr(&mut self) -> INTR_W<0> {
+        INTR_W::new(self)
+    }
+    #[doc = "Writes raw bits to the register."]
+    #[inline(always)]
+    pub unsafe fn bits(&mut self, bits: u32) -> &mut Self {
+        self.0.bits(bits);
+        self
+    }
+}
 #[doc = "Interrupt Status (raw)  
 
-This register you can [`read`](crate::generic::Reg::read). See [API](https://docs.rs/svd2rust/#read--modify--write-api).  
+This register you can [`read`](crate::generic::Reg::read), [`write_with_zero`](crate::generic::Reg::write_with_zero), [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`modify`](crate::generic::Reg::modify). See [API](https://docs.rs/svd2rust/#read--modify--write-api).  
 
 For information about available fields see [intr](index.html) module"]
 pub struct INTR_SPEC;
@@ -46,6 +95,12 @@ impl crate::RegisterSpec for INTR_SPEC {
 #[doc = "`read()` method returns [intr::R](R) reader structure"]
 impl crate::Readable for INTR_SPEC {
     type Reader = R;
+}
+#[doc = "`write(|w| ..)` method takes [intr::W](W) writer structure"]
+impl crate::Writable for INTR_SPEC {
+    type Writer = W;
+    const ZERO_TO_MODIFY_FIELDS_BITMAP: Self::Ux = 0;
+    const ONE_TO_MODIFY_FIELDS_BITMAP: Self::Ux = 0xffff;
 }
 #[doc = "`reset()` method sets INTR to value 0"]
 impl crate::Resettable for INTR_SPEC {
