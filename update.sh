0930c9ffa9e7470c44c5b7a899143649b1bb8867
Jan Niehusmann <jan@gondor.com>
2024-10-01 19:49:29 +0000

    Update form to 0.12.1

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ SVDTOOLS="${SVDTOOLS:-svdtools}"
 set -ex
 
 cargo install --version 0.33.4 svd2rust
-cargo install --version 0.11.1  form
+cargo install --version 0.12.1 form
 rustup component add rustfmt
 if [ "$SVDTOOLS" == "svdtools" ]; then
     cargo install --version 0.3.18 svdtools
